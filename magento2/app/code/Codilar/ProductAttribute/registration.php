2f3f1ead5709f52333daa7826a891c87951a2e4d
Ahmed Arsalan <arsalaanmuallim28@gmail.com>
2018-03-16 13:21:45 +0530

    productAttribute

---
+++ magento2/app/code/Codilar/ProductAttribute/registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ahmed
+ * Date: 16/3/18
+ * Time: 12:44 PM
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+        \Magento\Framework\Component\ComponentRegistrar::MODULE,
+        'Codilar_ProductAttribute',
+        __DIR__
+    );
