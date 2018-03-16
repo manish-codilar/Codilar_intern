6fc339f0657730f44506d51430d1f329e189d8c8
mikguru26 <manishkumar@codilar.comgit config --global user.name mikguru26git config --global user.email manishkumar@codilar.com>
2018-03-16 13:19:37 +0530

    module

--- magento2/app/code/Codilar/CustomerAttribute/registration.php
+++ magento2/app/code/Codilar/CustomerAttribute/registration.php
@@ -1,6 +1,6 @@
 <?php
     \Magento\Framework\Component\ComponentRegistrar::register(
         \Magento\Framework\Component\ComponentRegistrar::MODULE,
-        'Codilar_HelloWorld',
+        'Codilar_CustomerAttribute',
         __DIR__
     );
