9ebc0e517b5087eb07af932925afc3af1e8b01b7
mikguru26 <manishkumar@codilar.com>
2018-03-18 17:35:19 +0530

    seller

--- magento2/app/code/Codilar/Seller/Controller/Index/Index.php
+++ magento2/app/code/Codilar/Seller/Controller/Index/Index.php
@@ -5,29 +5,16 @@ class Index extends \Magento\Framework\App\Action\Action
 {
     protected $_pageFactory;
 
-    protected $_postFactory;
-
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
-        \Magento\Framework\View\Result\PageFactory $pageFactory,
-        \Codilar\Seller\Model\PostFactory $postFactory
-    )
+        \Magento\Framework\View\Result\PageFactory $pageFactory)
     {
         $this->_pageFactory = $pageFactory;
-        $this->_postFactory = $postFactory;
         return parent::__construct($context);
     }
 
     public function execute()
     {
-        $product = $this->_postFactory->create();
-        $collection = $product->getCollection();
-        foreach($collection as $item){
-            echo "<pre>";
-            print_r($item->getData());
-            echo "</pre>";
-        }
-        exit();
         return $this->_pageFactory->create();
     }
 }
